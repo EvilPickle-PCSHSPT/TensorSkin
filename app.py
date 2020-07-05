082c49387dc6c73e9e523aa3de743af9d5874e3c
Waranthorn Chansawang <filmer2002@hotmail.com>
2020-07-05 11:27:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,23 +15,7 @@ import base64
 
 IMG_WIDTH, IMG_HEIGHT = 224, 224
 
-def ArithmeticMean_ensemble(test_data, model_1, model_2, model_3):
-  model_1 = tf.keras.models.load_model(model_1)
-  model_2 = tf.keras.models.load_model(model_2)
-  model_3 = tf.keras.models.load_model(model_3)
-  ans_1 = model_1.predict(test_data)
-  ans_2 = model_2.predict(test_data)
-  ans_3 = model_3.predict(test_data)
-  all_answer_list = []
-  for i in range(len(ans_1)):
-    answer_list = []
-    for j in range(len(ans_1[i])):
-      mean_ans = (ans_1[i][j] + ans_2[i][j] + ans_3[i][j])/3
-      answer_list.append(mean_ans)
-    all_answer_list.append(answer_list)
-  return all_answer_list
-
-final_answer_array = np.asarray(final_answer)
+final_answer_array = np.asarray(mean_answer)
 
 UPLOAD_FOLDER = 'upload_folder/'
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'JPG', 'JPEG', 'png'])
@@ -41,25 +25,38 @@ app = Flask(__name__)
 def get_as_base64(url):
     return base64.b64encode(requests.get(url).content)
 
-def predict(file):
-    x = load_img(file, target_size=(IMG_WIDTH, IMG_HEIGHT))
-    x = img_to_array(x)
-    x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-    array = model.predict(x)
-    result = array[0]
-    answer = np.argmax(result)
+def ensemble_predict(test_data):
+    test_data = load_img(test_data, target_size=(IMG_WIDTH, IMG_HEIGHT))
+    test_data = img_to_array(test_data)
+    test_data = np.true_divide(test_data, 255)
+    test_data = np.expand_dims(test_data, axis=0)
 
-    if answer == 0:
+    model_1 = tf.keras.models.load_model('model/densenet121_model_1.h5')
+    model_2 = tf.keras.models.load_model('model/densenet121_model_2.h5')
+    model_3 = tf.keras.models.load_model('model/densenet121_model_3.h5')
+    ans_1 = model_1.predict(test_data)
+    ans_2 = model_2.predict(test_data)
+    ans_3 = model_3.predict(test_data)
+    all_answer_list = []
+    for i in range(len(ans_1)):
+        answer_list = []
+        for j in range(len(ans_1[i])):
+            mean_ans = (ans_1[i][j] + ans_2[i][j] + ans_3[i][j])/3
+            answer_list.append(mean_ans)
+        all_answer_list.append(answer_list)
+        result = all_answer_list[0]
+    mean_answer = np.argmax(result)
+
+    if mean_answer == 0:
         print('Label: Atopic Dermatitis')
-    elif answer == 1:   
+    elif mean_answer == 1:   
         print('Label: Normal')
-    elif answer == 2:
+    elif mean_answer == 2:
         print('Label: Psoriasis')
-    elif answer == 3:
+    elif mean_answer == 3:
         print('Label: Seborrhoeic Keratosis')
         
-    return answer
+    return mean_answer
 
 def my_random_string(string_length=10):
     """Returns a random string of length string_length."""
@@ -91,7 +88,7 @@ def upload_file():
 
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
-            result = predict(file_path)
+            result = ensemble_predict(file_path)
             if result == 0:
                 label = 'Atopic Dermatitis'
             elif result == 1:
