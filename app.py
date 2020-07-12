a3039fdcf2ff3549cd889d0f362bf8b71c968b86
Waranthorn Chansawang <filmer2002@hotmail.com>
2020-07-12 22:03:19 +0700

    update

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import base64
 
 IMG_WIDTH, IMG_HEIGHT = 224, 224
 
-final_answer_array = np.asarray(mean_answer)
+# final_answer_array = np.asarray(mean_answer)
 
 UPLOAD_FOLDER = 'upload_folder/'
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'JPG', 'JPEG', 'png'])
@@ -31,9 +31,9 @@ def ensemble_predict(test_data):
     test_data = np.true_divide(test_data, 255)
     test_data = np.expand_dims(test_data, axis=0)
 
-    model_1 = tf.keras.models.load_model('model/densenet121_model_1.h5')
-    model_2 = tf.keras.models.load_model('model/densenet121_model_2.h5')
-    model_3 = tf.keras.models.load_model('model/densenet121_model_3.h5')
+    model_1 = tf.keras.models.load_model('model/TensorSkin_model_1.h5')
+    model_2 = tf.keras.models.load_model('model/TensorSkin_model_2.h5')
+    model_3 = tf.keras.models.load_model('model/TensorSkin_model_3.h5')
     ans_1 = model_1.predict(test_data)
     ans_2 = model_2.predict(test_data)
     ans_3 = model_3.predict(test_data)
